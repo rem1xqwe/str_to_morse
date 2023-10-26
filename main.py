77fcc963b0349a852b9142c331d6dc79db2e5453
ReM1X <ticcicostia3310@gmail.com>
2023-10-26 15:17:14 +0300

    final_morse_code

---
+++ main.py
@@ -0,0 +1,23 @@
+
+user_input = input("Enter te text to be translated: ").upper()
+
+
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.'
+}
+
+
+massage = ""
+
+for letter in user_input:
+    if letter == " ":
+        massage += " "
+    else:
+        output = morse_code_dict[letter]
+        massage += f"{output + ' '}"
+print(f"Converted massage: {massage}")
